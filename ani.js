38c9232812b691daac91cb282060926ef4164d06
aryanbhale <aryanbhale2004@gmail.com>
2025-07-28 16:04:25 +0530

    update - 0.0.20

--- ani.js
+++ ani.js
@@ -7,12 +7,3 @@
 //     text: "Inderdeep Infra India Limited.",
 // });
 
-const TESTER = document.getElementById('tester');
-          Plotly.plot(TESTER, [{
-               x: [1, 2, 3, 4, 5],
-               y: [1, 2, 4, 8, 16]
-          }], {
-               margin: { t: 0 }
-          }, { showSendToCloud: true });
-
-          console.log(Plotly.BUILD);
